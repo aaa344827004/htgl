e7e0d6fa7e60f4467178efb9028f34f6f96e7e51
wwz <344827004@qq.com>
2020-10-21 15:41:01 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+// axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 new Vue({
   router,
